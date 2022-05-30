b64a98d5dd8281a5f683388a7a4addc3a4eb99ba
alicekres <alice.kreslins@gmail.com>
2022-05-30 18:01:39 +0100

    add routes, controller, model - not finished yet

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const stories = require('./routes/stories');
 
 app.use(cors());
 app.use(express.json());
+app.use('/stories', storiesRoutes);
 
 app.get('/', (req, res) => {
     res.send('Ready to tackle the challenge!?');
---
+++ api/models/story.js
@@ -0,0 +1,7 @@
+class Story {
+    constructor(data) {
+        this.id = data.id;
+        this.title = data.title;
+        this.story = data.story;
+    }
+}
