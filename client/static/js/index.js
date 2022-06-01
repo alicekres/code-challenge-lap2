2303f7179e143478cdeaedb97743d9a161028e80
alicekres <alice.kreslins@gmail.com>
2022-06-01 08:56:40 +0100

    index.js original

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,60 +1,60 @@
-// const firstNames = [
-//     'Hope',
-//     'Launa',
-//     'Katherine',
-//     'Walker',
-//     'Moses',
-//     'Tayna',
-//     'Rosia',
-//     'Yahaira',
-//     'Tommy',
-//     'Elwanda',
-//     'Clorinda',
-//     'Sheron',
-//     'Kayla',
-//     'Clementina',
-//     'Rene',
-//     'Rex',
-//     'Kathy',
-//     'Latoya',
-//     'Shirleen',
-//     'Shoshana',
-// ];
+const firstNames = [
+    'Hope',
+    'Launa',
+    'Katherine',
+    'Walker',
+    'Moses',
+    'Tayna',
+    'Rosia',
+    'Yahaira',
+    'Tommy',
+    'Elwanda',
+    'Clorinda',
+    'Sheron',
+    'Kayla',
+    'Clementina',
+    'Rene',
+    'Rex',
+    'Kathy',
+    'Latoya',
+    'Shirleen',
+    'Shoshana',
+];
 
-// const secondNames = [
-//     'Chauncey',
-//     'Houchins',
-//     'Lama',
-//     'Frasca',
-//     'Houston',
-//     'Ake',
-//     'Shankles',
-//     'Cantor',
-//     'Mizell',
-//     'Cleland',
-//     'Maltby',
-//     'Tirrell',
-//     'Cary',
-//     'Mace',
-//     'Horta',
-//     'Carlile',
-//     'Deegan',
-//     'Torrez',
-//     'Humfeld',
-//     'Helgeson',
-// ];
+const secondNames = [
+    'Chauncey',
+    'Houchins',
+    'Lama',
+    'Frasca',
+    'Houston',
+    'Ake',
+    'Shankles',
+    'Cantor',
+    'Mizell',
+    'Cleland',
+    'Maltby',
+    'Tirrell',
+    'Cary',
+    'Mace',
+    'Horta',
+    'Carlile',
+    'Deegan',
+    'Torrez',
+    'Humfeld',
+    'Helgeson',
+];
 
-// const getRandomNumber = (max) => Math.floor(Math.random() * max);
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
-// const getRandomName = () =>
-//     `${firstNames[getRandomNumber(firstNames.length)]} ${
-//         secondNames[getRandomNumber(secondNames.length)]
-//     }`;
+const getRandomName = () =>
+    `${firstNames[getRandomNumber(firstNames.length)]} ${
+        secondNames[getRandomNumber(secondNames.length)]
+    }`;
 
-// const setRandomName = () => {
-//     document.getElementById('random-name').innerText = getRandomName();
-// };
+const setRandomName = () => {
+    document.getElementById('random-name').innerText = getRandomName();
+};
 
-// document.getElementById('generate').addEventListener('click', setRandomName);
+document.getElementById('generate').addEventListener('click', setRandomName);
 
-// setRandomName();
+setRandomName();
